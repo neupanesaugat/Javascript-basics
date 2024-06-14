4e9f8cbbcf7cf422acb9f88267d735f6ba8b1c68
neupanesaugat <saugatneupane50@gmail.com>
2024-06-14 20:40:59 +0545

    first commit

---
+++ a.hello.js
@@ -0,0 +1,4 @@
+//? First Step of programming
+//? Printing "Hello World"
+
+console.log("Hello World");
---
+++ b.variable.and.constants.js
@@ -0,0 +1,37 @@
+//?let variableName = value
+
+let studentName = "Saugat Neupane";
+console.log(studentName);
+
+//?naming convention
+//?1)variable name cannot be reserved keyword
+//?such as for,let,const,while,if,switch
+//?2) variable name cannot start with numbers
+//?such as 12num=4
+//?3)variable cannot contain special character(except _ and $)
+
+let _ = 5;
+console.log(_);
+let $ = 2;
+console.log($);
+
+let first_name = "Ram";
+
+//?4) variable name should be descriptive
+
+let t = 30;
+
+//?t should be defined
+//?5) varible name should be camel case
+
+let firstName = "A";
+let isGraduated = "false";
+
+//?Constant
+const x = 7;
+x = 10;
+console.log(x);
+
+//?universal constant are written in uppercase
+const PI = Math.PI;
+console.log(PI);
