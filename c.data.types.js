d58fbc1e78b4d17c5b072b42be2619d356efecb0
Saugat Neupane <saugatneupane50@gmail.com>
2024-06-16 20:58:41 +0545

    c.data.types.js

--- c.data.types.js
+++ c.data.types.js
@@ -11,65 +11,65 @@
 //? 1) string
 //? "",'',`` (inside any of these)
 
-// let userName = "raman@123";
-// console.log(userName);
-// console.log(typeof userName);
+ let userName = "raman@123";
+ console.log(userName);
+/ console.log(typeof userName);
 
-// let x = "5";
-// console.log(x);
-// console.log(typeof x);
+ let x = "5";
+ console.log(x);
+console.log(typeof x);
 
 //? 2)number
-// let x = 1;
-// console.log(x);
-// console.log(typeof x);
+ let x = 1;
+ console.log(x);
+console.log(typeof x);
 
-// let x = Infinity;
-// console.log(x);
-// console.log(typeof x);
+ let x = Infinity;
+ console.log(x);
+ console.log(typeof x);
 
-// let x = 4 / 0;
-// console.log(x);
-// console.log(typeof x);
+ let x = 4 / 0;
+ console.log(x);
+ console.log(typeof x);
 
 //?NAN = Not A Number
-// let x = "abc" / 2;
-// console.log(x);
-// console.log(typeof x);
+ let x = "abc" / 2;
+ console.log(x);
+ console.log(typeof x);
 //?float is also number
-// let x = 4.487584;
-// console.log(x);
-// console.log(typeof x);
+ let x = 4.487584;
+ console.log(x);
+ console.log(typeof x);
 
 //? 3) boolean => true/false
 
-// let isSunnyDay = false;
-// console.log(isSunnyDay, typeof isSunnyDay);
+ let isSunnyDay = false;
+ console.log(isSunnyDay, typeof isSunnyDay);
 
 //? 4) undefined
 //? js cannot guess data type
 
-// let x;
-// console.log(x, typeof x);
+ let x;
+ console.log(x, typeof x);
 
-// let x = undefined;
-// console.log(x, typeof x);
+ let x = undefined;
+ console.log(x, typeof x);
 
 //? 5) null
 
-// let x = null;
-// console.log(x, typeof x);
+ let x = null;
+ console.log(x, typeof x);
 
 //? 6) object
 
-// const studentDetail = {
-//   fName: "Saugat",
-//   lName: "Neupane",
-//   address: "A",
-// };
-// console.log(studentDetail, typeof studentDetail);
+ const studentDetail = {
+   fName: "Saugat",
+   lName: "Neupane",
+   address: "A",
+ };
+ console.log(studentDetail, typeof studentDetail);
 
 //?array is also an object
 
-// const numList = [25, 55, 65, 11, 23];
-// console.log(numList, typeof numList);
+ const numList = [25, 55, 65, 11, 23];
+ console.log(numList, typeof numList);
