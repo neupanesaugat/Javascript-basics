b4b0640b4a586b3cea0b8ecd35cc1f8af6968eb0
Saugat Neupane <saugatneupane50@gmail.com>
2024-06-29 23:26:23 +0545

    truthy falsy values in js

---
+++ i.truthy.falsy.values.js
@@ -0,0 +1,16 @@
+//?truthy and falsy values
+
+//?falsy values
+
+//?when these values come in condition they are equivalent to false values
+//?false,null,undefined,0,-0,Nan.""
+
+//?truthy values => all values except falsy values
+
+let condition = 1;
+
+// console.log(undefined ? "truthy" : "falsy");
+
+// if (10) {
+//   console.log("hit");
+// }
